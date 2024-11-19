9257fd0567f5294b7d4b1853d7ea8414176264c9
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-11-19 10:34:22 +0700

    edit route path

--- resources/views/includes/sidenav.blade.php
+++ resources/views/includes/sidenav.blade.php
@@ -31,7 +31,7 @@
                         </div>
                         <ul class="py-1" role="none">
                             <li>
-                                <a href="{{ route('profile.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">My Profile</a>
+                                {{-- <a href="{{ route('profile.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">My Profile</a> --}}
                             </li>
                             <li>
                                 <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-red-700 hover:bg-gray-100" role="menuitem">Sign out</a>
@@ -49,7 +49,7 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
         <ul class="space-y-2 font-medium">
-            <li>
+            {{-- <li>
                 <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
                     <svg class="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                         <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
@@ -57,7 +57,7 @@
                     </svg>
                     <span class="ms-3">Dashboard</span>
                 </a>
-            </li>
+            </li> --}}
             <li>
                 <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
                     <i class="fa-solid fa-file-invoice text-gray-500 text-2xl"></i>
