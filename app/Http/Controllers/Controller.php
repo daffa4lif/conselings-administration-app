50a458ff89463f3c74630a4055b311798caedb97
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-11-19 14:04:05 +0700

    add global response server errors

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public static function redirectResponseServerError()
+    {
+        return back()->with('error', 'server error. coba lagi nanti!');
+    }
 }
