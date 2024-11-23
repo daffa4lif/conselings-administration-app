401dc704bbbc669ff0f11520357e54c4b2212a03
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-11-23 19:06:19 +0700

    fix: error issue at link to create data

--- resources/views/pages/home-visit/index.blade.php
+++ resources/views/pages/home-visit/index.blade.php
@@ -4,7 +4,7 @@
 <header class="mb-5">
     <x-main-header title="List Kunjungan Rumah" />
     <div class="flex justify-end">
-        <a href="#" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" >
+        <a href="{{ route('home-visit.create') }}" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" >
             Tambah
         </a>
 
