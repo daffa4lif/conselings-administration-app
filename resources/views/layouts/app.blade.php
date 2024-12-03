5dfe729c8018a34be253475bace9a4263db59943
Daffa Alif Murtaja <daffa4lif@gmail.com>
2024-12-03 22:41:34 +0700

    add apex chart cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
     </div>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     @stack('script')
 </body>
 </html>
